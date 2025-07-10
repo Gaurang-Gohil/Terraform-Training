d3c9bedc166a35bbff324c0ac1847e9b548aee12
Gaurang-Gohil <work.gaurang2005@gmail.com>
2025-07-10 15:44:53 -0700

    changed a code in 02-overview/main .tf

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,25 @@ terraform {
 }
 
 provider "aws" {
+  alias  = "east"
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
+
+resource "aws_instance" "east" {
+  count         = 3
+  provider      = aws.east
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "west" {
+  count         = 2
+  provider      = aws.west
+  ami           = "ami-0c2d3e23f757b5d84" # Ubuntu 20.04 LTS // us-west-2
   instance_type = "t2.micro"
 }
